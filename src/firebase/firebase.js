7137fd7a216722018a1c0e184036c925e1d4774a
Fin Moorhouse <5040505+finmoorhouse@users.noreply.github.com>
2020-06-02 21:41:02 +0100

    Still trying to fix Firebase variables.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,20 +1,19 @@
 import * as firebase from "firebase";
 
 //import variables from "./firebaseVariables";
-console.log(process.env.NODE_ENV);
+console.log(process.env.API_KEY);
 let variables;
 let firebaseConfig;
 
-console.log("not production");
-if(process.env.NODE_ENV=='production') {
+if(process.env.NODE_ENV==='production') {
   firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STOREAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STOREAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
   };
 } else {
   variables = require("./firebaseVariables").default;
